3406e5b56862962003b32431c079effbf3633207
VietAnhh <viet.le175@gmail.com>
2016-10-12 19:56:33 -0400

    fixed up css

--- MealHackMobile/app/pages/home/home.ts
+++ MealHackMobile/app/pages/home/home.ts
@@ -65,8 +65,49 @@ export class HomePage {
   }
 
   goBackToStart(){
-    console.log("bruh");
-    TweenLite.to(this.RestaurantNameOrder[this.currentRestaurant].nativeElement, 0.5 ,{height: "auto"});
+    // console.log("bruh");
+    // TweenLite.to(this.RestaurantNameOrder[this.currentRestaurant].nativeElement, 0.5 ,{height: "auto"});
+
+
+    console.log(this.currentRestaurant);
+    //HIDE THE MENU LIST
+    this.RestaurantNameOrder[this.currentRestaurant].nativeElement.children[2].style.display = "none";
+    //IMG
+    let img = this.RestaurantNameOrder[this.currentRestaurant].nativeElement.children[1];
+    TweenLite.to(img , 0.5, {height:"130px", ease:Circ.easeIn, onComplete: ImgComplete});
+    //CLOSE
+    let close = window.document.getElementById("close-marker");
+    TweenLite.to(close, 1, {left:"-400px",ease:Circ.easeOut, rotation: 1440, onComplete: CloseFinish});
+
+
+    //BRING BACK OTHER RESTAURANT
+    for(let i = 0; i < this.RestaurantNameOrder.length ; i++){
+      if(i === this.currentRestaurant){
+        console.log("yes");
+      }else{
+        this.RestaurantNameOrder[i].nativeElement.style.display = "";
+        this.RestaurantNameOrder[i].nativeElement.style.right = "";
+        this.RestaurantNameOrder[i].nativeElement.style.opacity = "1";
+        TweenLite.from(this.RestaurantNameOrder[i].nativeElement, 0.5, {right:"400px" ,opacity: 0, ease:Circ.easeOut, onComplete:MenuFinish, onCompleteParams:[this.RestaurantNameOrder[i]]});
+
+      }
+    }
+
+
+    function CloseFinish(){
+      // close.style.opacity = "1";
+      close.style.left = "85%";
+      close.style.display = "none";
+    }
+
+    function MenuFinish(obj){
+    }
+
+    function ImgComplete(){
+      img.style.height = "auto";
+    }
+
+
     // for(let i = 0; i < this.RestaurantNameOrder.length; i++){
 
     // }
@@ -82,13 +123,39 @@ export class HomePage {
   }
 
   expandRestaurant(obj){
-    console.log(obj.nativeElement.children[3]);
-    let img = obj.nativeElement.children[3];
-    let text = obj.nativeElement.children[1];
-    obj.nativeElement.children[0].style.display = 'none';
-    obj.nativeElement.children[2].style.display = 'none';
+    // console.log(obj.nativeElement.children[]);
+    let img = obj.nativeElement.children[1];
+    let text = obj.nativeElement.children[0];
+    let card = obj.nativeElement;
+    let close = window.document.getElementById("close-marker");
+    let foodlist = window.document.getElementsByClassName("foodlist");
+    close.style.display = "";
+    console.log(close);
+    // obj.nativeElement.children[0].style.display = 'none';
+    // obj.nativeElement.children[2].style.display = 'none';
+    obj.nativeElement.children[2].style.display = '';
+    //Text
+    TweenLite.to(text, 0.5, {top:"0", "padding-bottom": "15px", ease:Circ.easeOut});
+    //Img
     TweenLite.to(img, 0.5, {height:"250px", ease:Circ.easeOut});
+    //Card
+    TweenLite.to(card, 0.5, {height:"auto" ,ease:Circ.easeOut});
+    //Close Marker
+    TweenLite.from(close, 1, {left:"-400px", opacity: 0, ease:Circ.easeOut, rotation: 1000,delay: 0.5});
+    // Food List
+    // console.log(foodlist);
+    // foodlist[0].style.display = "";
+    TweenLite.from(foodlist[0], 0.5, {right:"500px", opacity: 0, ease:Circ.easeOut});
 
+
+
+
+
+  }
+
+
+  safetyButton(){
+    this.goBackToStart();
   }
 
 }
