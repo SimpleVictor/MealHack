5870008be86907693a3719c4cb13568b2fc102cf
VietAnhh <viet.le175@gmail.com>
2016-10-15 02:51:47 -0400

    small typo

--- MealHackMobile/app/providers/sqlstorage.ts
+++ MealHackMobile/app/providers/sqlstorage.ts
@@ -11,7 +11,7 @@ export class SqlStorageService {
   webDB;
 
   constructor(private platform: Platform) {
-    let whichPlat = platform.platforms;
+    let whichPlat = platform.platforms();
     console.log(whichPlat);
     if(whichPlat[0] === 'cordova'){
       console.log("your on the iphone");
